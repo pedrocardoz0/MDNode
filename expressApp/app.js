4350ab3a5dd5255e5f9732ddc91aea24e278379b
Pedro Henrique Cardozo Ferreira <p-cardozo@hotmail.com>
2020-06-15 10:49:31 -0300

    Add Something

--- expressApp/app.js
+++ expressApp/app.js
@@ -7,6 +7,12 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var mongoose = require('mongoose')
+var mongoDB = 'mongodb://127.0.0.1/my_database'
+mongoose.connect(mongoDB, { useNewUrlParser: true })
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
 var app = express();
 
 // view engine setup
